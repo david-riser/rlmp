6f583bdd3f1156b3ac882c3fab1cdc4d4d847296
Ubuntu <ubuntu@ip-172-31-43-72.ec2.internal>
2020-07-24 18:51:54 +0000

    Added some dqn stuff

---
+++ common/dqn.py
@@ -0,0 +1,86 @@
+""" 
+Network architecture functions from:
+https://github.com/higgsfield/RL-Adventure/blob/master/1.dqn.ipynb
+
+"""
+
+import numpy as np
+import random 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd 
+import torch.nn.functional as F
+
+USE_CUDA = torch.cuda.is_available()
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
+class DQN(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(DQN, self).__init__()
+
+        self.num_actions = num_actions
+        self.num_inputs = num_inputs
+        
+        self.layers = nn.Sequential(
+            nn.Linear(self.num_inputs, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.num_actions)
+        )
+        
+    def forward(self, x):
+        return self.layers(x)
+    
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state   = Variable(torch.FloatTensor(state).unsqueeze(0), volatile=True)
+            q_value = self.forward(state)
+            action  = q_value.max(1)[1].data[0].cpu().numpy()
+        else:
+            action = random.randrange(self.num_actions)
+        return action
+
+
+class CnnDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(CnnDQN, self).__init__()
+        
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions)
+        )
+        
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+    
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+    
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state   = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0))
+            q_value = self.forward(state)
+            action  = q_value.max(1)[1].data[0].cpu().numpy()
+        else:
+            action = random.randrange(self.num_actions)
+        return action
+    
+
--- dqn.py
+++ dqn.py
@@ -3,158 +3,44 @@ import glob
 import gym
 import numpy as np
 import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd 
+import torch.nn.functional as F
 import wandb
+
 from gym import wrappers
 from pyvirtualdisplay import Display
-from utils.replay import ReplayBuffer
+from common.replay import ReplayBuffer
+from common.dqn import Variable, DQN, CnnDQN, USE_CUDA
 
-from tensorflow.keras import Model
-from tensorflow.keras.layers import (Activation, Conv2D, Dense, Flatten,
-                                     Input, MaxPooling2D)
-from tensorflow.keras.optimizers import Adam
 
 
 def get_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('--env', type=str, default='CartPole-v0')
-    ap.add_argument('--episodes', type=int, default=100)
+    ap.add_argument('--max_frames', type=int, default=10000)
+    ap.add_argument('--batch_size', type=int, default=32)
+    ap.add_argument('--gamma', type=float, default=0.99)
+    ap.add_argument('--eps_start', type=float, default=1.)
+    ap.add_argument('--eps_final', type=float, default=0.1)
+    ap.add_argument('--eps_decay', type=float, default=2500)
     return ap.parse_args()
 
 def setup_wandb(args):
     config = dict(
         env = args.env,
-        episodes = args.episodes
+        max_frames = args.max_frames
     )
     wandb.init(
         project='rlmp',
-        notes='Random Agent',
-        tags=['Random'],
+        notes='Basic DQN',
+        tags=['DQN'],
         config=config
     )
 
 
-def build_network(input_shape, output_shape):
-    """ Build a fully connected network or 
-    a convolutional network based on the shape
-    of the observation space. """
-    if len(input_shape) > 1:
-        print("Building convnet with shapes: {} -> {}".format(
-            input_shape, output_shape
-        ))
-        return _build_convnet(input_shape, output_shape)
-    else:
-        print("Building net with shapes: {} -> {}".format(
-            input_shape, output_shape
-        ))
-        return _build_fc(input_shape, output_shape)
-
-    
-def _build_convnet(input_shape, output_shape):
-    inputs = Input(input_shape)
-
-    x = Conv2D(8, 3)(inputs)
-    x = Activation('relu')(x)
-    x = Conv2D(8, 3)(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D()(x)
-
-    x = Conv2D(8, 3)(x)
-    x = Activation('relu')(x)
-    x = Conv2D(8, 3)(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D()(x)
-
-    x = Conv2D(8, 3)(x)
-    x = Activation('relu')(x)
-    x = Conv2D(8, 3)(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D()(x)
-    x = Flatten()(x)
-    x = Dense(64)(x)
-    
-    outputs = Dense(output_shape)(x)
-    
-    return Model(inputs, outputs)
-
-
-def _build_fc(input_shape, output_shape):
-        inputs = Input(input_shape)
-        x = Dense(64)(inputs)
-        x = Dense(64)(x)
-        outputs = Dense(output_shape)(x)
-        return Model(inputs, outputs)
-
-
-class Agent:
-
-    def __init__(self, action_space, observation_space, max_size, batch_size):
-        self.batch_size = batch_size
-        self.action_space = action_space
-        self.observation_space = observation_space
-        self.memory = ReplayBuffer(n_actions=self.action_space.n,
-                                   obs_shape=self.observation_space.shape,
-                                   max_size=max_size)
-        self.policy = build_network(self.observation_space.shape, self.action_space.n)
-        self.target = build_network(self.observation_space.shape, self.action_space.n)
-        self._compile_models()
-
-
-    def _compile_models(self):
-        optimizer = Adam(0.001)
-        self.policy.compile(optimizer=optimizer, loss='mse')
-        self.target.compile(optimizer=optimizer, loss='mse')
-        self.target.set_weights(self.policy.get_weights())
-
-
-    def choose_action(self, observation):
-         
-        if random.random() < 0.9:
-            print("Taking best action")
-            action = self._choose_best_action(observation)
-        else:
-            print("Taking random action")
-            action = self._choose_random_action(observation)
-
-    def _choose_random_action(self, observation):
-        return self.action_space.sample()
-
-
-    def _choose_best_action(self, observation):
-        return np.argmax(self.policy.predict(observation.reshape(
-            1, *observation.shape
-        )))
-
-
-    def store(self, action, state, next_state, reward, done):
-        self.memory.store(action, state, next_state, reward, done)
-    
-    def learn(self, i):
-
-        # Time to do the updates
-        if i % 5 == 0:
-            self.target.set_weights(self.policy.get_weights())
-
-        if len(self.memory) >= self.batch_size:
-            print("Learning step" )
-            actions, states, next_states, rewards, dones = self.memory.sample(self.batch_size)
-            print(actions)
-            print(states)
-            print(next_states)
-            print(rewards)
-            print(dones)
-            
-            # Get the states and predict the Q-values
-            # from the batch.
-            policy_preds = self.policy.predict(states.reshape(1,*states.shape))
-            target_preds = self.target.predict(next_states.reshape(1,*next_states.shape))
-            rewards *= (1. - dones)
-
-            # This might not be the correct update procedure yet,
-            # but you have to start somewhere!
-            self.policy.train_on_batch(rewards, target_preds)        
-
-            
-        
 if __name__ == "__main__":
 
     args = get_args()
@@ -172,54 +58,75 @@ if __name__ == "__main__":
     virtual_display = Display(visible=0, size=(320,240))
     virtual_display.start()
 
-    
-    agent = Agent(
-        action_space=env.action_space,
-        observation_space=env.observation_space,
-        max_size=10000,
-        batch_size=32
-    )
 
+    # Setup agent
+    if len(env.observation_space.shape) == 1:
+        model = DQN(
+            num_inputs=env.observation_space.shape[0],
+            num_actions=env.action_space.n
+        )
+    else:
+        model = CnnDQN(
+            input_shape=env.observation_space.shape,
+            num_actions=env.action_space.n
+        )
 
-    while not agent.memory.is_full:
-        done = False
-        observation = env.reset()        
-        while not done:
-            action = env.action_space.sample()
-            obs, reward, done, _ = env.step(action)
-            agent.store(action, observation, obs, reward, done)
-            observation = obs
+    if USE_CUDA:
+        model = model.cuda()
         
-    for episode in range(args.episodes):
-        observation = env.reset()
+    optimizer = optim.Adam(model.parameters())
+    replay_buffer = ReplayBuffer(10000)
+    num_frames = 0
+    while num_frames < args.max_frames:
 
+        state = env.reset()
         done = False
-        i = 0
-        scores = []
+        ep_reward = 0 
         while not done:
-            # action = env.action_space.sample()
-            action = agent.choose_action(observation)
-            print("Action ", action)
+            epsilon = args.eps_final + (args.eps_start - args.eps_final) * np.exp(-1. * num_frames / args.eps_decay)
+            action = model.act(state,epsilon)
+            next_state, reward, done, _ = env.step(action)
+            replay_buffer.add(state, action, reward, next_state, done)
+            wandb.log({'reward':reward, 'epsilon':epsilon})
+            num_frames += 1
+            ep_reward += reward
+            state = next_state
 
-            next_observation, reward, done, _ = env.step(action)
+            # Update network
+            states, actions, rewards, next_states, dones = replay_buffer.sample(args.batch_size)
 
-            agent.store(action, observation, next_observation, reward, done)
-            agent.learn(i)
+            # Make into torch variables to calculate
+            # the loss with
+            states = Variable(torch.FloatTensor(np.float32(states)))
+            actions = Variable(torch.LongTensor(actions))
+            rewards = Variable(torch.FloatTensor(rewards))
+            next_states = Variable(torch.FloatTensor(np.float32(next_states)))
+            dones = Variable(torch.FloatTensor(dones))
+
+            # Predict value for these state/action pairs
+            # and next state/action pairs. 
+            q_values = model(states)
+            next_q_values = model(next_states)
+
+            # Calculate value for actions we took
+            q_values = q_values.gather(1, actions.view(-1,1))
+            next_q_values = q_values.max(1).values
+
+            qhat = (rewards + args.gamma * next_q_values)
+            loss = (q_values - qhat.detach()).pow(2).mean()
             
-            log_data = dict(
-                reward = reward
-            )
-            wandb.log(log_data)
-            i += 1
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-            scores.append(reward)
-                    
-            observation = next_observation
-                    
-        print(np.mean(scores))
+            if num_frames % 100 == 0:
+                print("Frame {0}, Ep. Reward {1:6.4f}, Eps. {2:6.4f}, Q-avg: {3:6.4f}".format(
+                    num_frames, ep_reward, epsilon, q_values.mean().detach().cpu().numpy()
+                ))
             
         env.close()
-
-    # Upload the video
+        wandb.log({'ep_reward':ep_reward})
+        
+    # Upload the video 
     for movie in glob.glob(video_path + '/*.mp4'):
         wandb.log({'Video':wandb.Video(movie)})
