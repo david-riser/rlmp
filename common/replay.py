930d80e3e345d5316c6edec5a86227dc8cf5c9ba
Ubuntu <ubuntu@ip-172-31-43-72.ec2.internal>
2020-07-31 18:55:32 +0000

    Testing the addition of prioritized bugger

--- common/replay.py
+++ common/replay.py
@@ -7,7 +7,7 @@ class ReplayBuffer:
     def __init__(self, maxsize):
         self.buffer = []
         self.maxsize = maxsize
-
+        
     def add(self, state, action, reward, next_state, done):
         """ Add to the buffer and ensure that it is 
         not too full. """
@@ -21,7 +21,7 @@ class ReplayBuffer:
     def sample(self, batch_size):
         """ Get a batch of transitions. """
         idxes = np.random.choice(np.arange(len(self.buffer)), batch_size)
-
+        
         states, actions, rewards = [], [], []
         next_states, dones = [], []
         for idx in idxes:
@@ -36,3 +36,61 @@ class ReplayBuffer:
             np.array(rewards), np.array(next_states),
             np.array(dones)
         )
+
+
+class NaivePrioritizedReplayBuffer:
+    
+    """ Prioritized replay buffer following the RL-Adventure example
+    and the paper but written in my own style. """
+    
+    def __init__(self, maxsize, alpha=0.6):
+        self.buffer = []
+        self.maxsize = maxsize
+        self.alpha = alpha
+        self.priorities = []
+        self.last_batch_indices = None
+        
+    def add(self, state, action, reward, next_state, done):
+        """ Add to the buffer and ensure that it is 
+        not too full. """
+
+        max_prio = np.max(self.priorities) if self.buffer else 1.0
+        
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
+        self.priorities.append(max_prio)
+        
+        if len(self.buffer) > self.maxsize:
+            self.buffer.pop(0)
+            self.priorities.pop(0)
+            
+            
+    def sample(self, batch_size, beta=0.4):
+        """ Get a batch of transitions. """
+        probs = np.array(self.priorities) ** self.alpha
+        probs /= np.sum(probs)
+        idxes = np.random.choice(np.arange(len(self.buffer)), batch_size, p=probs)
+        total = len(self.buffer)
+
+        states, actions, rewards = [], [], []
+        next_states, dones, weights = [], [], []
+        for idx in idxes:
+            states.append(self.buffer[idx][0])
+            actions.append(self.buffer[idx][1])
+            rewards.append(self.buffer[idx][2])
+            next_states.append(self.buffer[idx][3])
+            dones.append(self.buffer[idx][4])
+            weights.append((total * probs[idx]) ** (-beta))
+
+        self.last_batch_indices = idxes
+        return (
+            np.array(states), np.array(actions),
+            np.array(rewards), np.array(next_states),
+            np.array(dones), np.array(weights) / np.max(weights)
+        )
+
+
+    def update_priorities(self, prios):
+        if self.last_batch_indices is not None:
+            for index, prio in zip(self.last_batch_indices, prios):
+                self.priorities[index] = prio
--- ddqn.py
+++ ddqn.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 import random
 import torch
 import torch.nn as nn
@@ -12,7 +13,7 @@ import wandb
 
 from gym import wrappers
 from pyvirtualdisplay import Display
-from common.replay import ReplayBuffer
+from common.replay import NaivePrioritizedReplayBuffer
 from common.ddqn import Variable, DDQN, CnnDDQN, USE_CUDA
 from stable_baselines.common.atari_wrappers import make_atari, wrap_deepmind
 
@@ -26,9 +27,12 @@ def get_args():
     ap.add_argument('--eps_start', type=float, default=1.00)
     ap.add_argument('--eps_final', type=float, default=0.01)
     ap.add_argument('--eps_decay', type=float, default=2500)
+    ap.add_argument('--beta_start', type=float, default=0.4)
+    ap.add_argument('--beta_frames', type=float, default=1000)
     ap.add_argument('--lr', type=float, default=0.00001)
     ap.add_argument('--replay_size', type=int, default=10000)
     ap.add_argument('--update_interval', type=int, default=1000)
+    
     return ap.parse_args()
 
 def setup_wandb(args):
@@ -125,7 +129,7 @@ if __name__ == "__main__":
     eval_frames = Variable(torch.FloatTensor(np.float32(eval_frames)))
     
     optimizer = optim.Adam(current_model.parameters(), lr=args.lr)
-    replay_buffer = ReplayBuffer(args.replay_size)
+    replay_buffer = NaivePrioritizedReplayBuffer(args.replay_size)
     num_frames = 0
     while num_frames < args.max_frames:
 
@@ -138,6 +142,7 @@ if __name__ == "__main__":
         ep_reward = 0 
         while not done:
             epsilon = args.eps_final + (args.eps_start - args.eps_final) * np.exp(-1. * num_frames / args.eps_decay)
+            beta = min(1.0, args.beta_start + num_frames * (1. - args.beta_start) / args.beta_frames)
             action = current_model.act(state,epsilon)
             next_state, reward, done, _ = env.step(action)
             next_state = np.array(next_state)
@@ -150,7 +155,7 @@ if __name__ == "__main__":
             state = next_state
 
             # Update network
-            states, actions, rewards, next_states, dones = replay_buffer.sample(args.batch_size)
+            states, actions, rewards, next_states, dones, weights = replay_buffer.sample(args.batch_size, beta)
 
             # Make into torch variables to calculate
             # the loss with
@@ -159,7 +164,8 @@ if __name__ == "__main__":
             rewards = Variable(torch.FloatTensor(rewards))
             next_states = Variable(torch.FloatTensor(np.float32(next_states)))
             dones = Variable(torch.FloatTensor(dones))
-
+            weights = Variable(torch.FloatTensor(weights))
+            
             # Predict value for these state/action pairs
             # and next state/action pairs. 
             q_values = current_model(states)
@@ -170,15 +176,21 @@ if __name__ == "__main__":
             next_q_values = next_q_values.max(1).values
 
             qhat = (rewards + args.gamma * next_q_values * (1 - dones))
-            loss = (q_values - qhat.detach()).pow(2).mean()
-            
+            loss = (q_values - qhat.detach()).pow(2)
+            loss = loss * weights
+            td_error = loss.detach().cpu().numpy() + 1e-5 
+            loss = loss.mean()
+
+            # This guy knows the last batch indices
+            replay_buffer.update_priorities(td_error)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             q_avg = current_model(eval_frames)
             q_avg = q_avg.cpu().detach().numpy().mean()
-            wandb.log({'return':ep_reward, 'epsilon':epsilon, 'q_avg':q_avg})
+            wandb.log({'return':ep_reward, 'epsilon':epsilon, 'q_avg':q_avg, 'beta':beta})
             
             if num_frames % 100 == 0:
                 print("Frame {0}, Ep. Reward {1:6.4f}, Eps. {2:6.4f}, Q-avg: {3:6.4f}".format(
@@ -202,7 +214,7 @@ if __name__ == "__main__":
         video_callable=lambda x: True
     )
 
-    eval_games = 10
+    eval_games = 40
     epsilon = 0.05
     scores = []
     with torch.no_grad():
@@ -236,6 +248,8 @@ if __name__ == "__main__":
             'eval_max':np.max(scores),
             'eval_mean':np.mean(scores),
             'eval_std':np.std(scores),
-            'eval_median':np.median(scores)
+            'eval_median':np.median(scores),
         }
     )
+
+    wandb.log({'eval_scores':scores})
