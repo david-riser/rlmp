e2e3133bd75e3dd17199cd1c58b3463e32b8bba0
Ubuntu <ubuntu@ip-172-31-80-116.ec2.internal>
2020-08-22 21:16:56 +0000

    Updating training script

--- rllib/train.py
+++ rllib/train.py
@@ -3,13 +3,25 @@ import glob
 import os
 import ray
 import ray.rllib.agents.dqn.apex as apex
+import time
 import wandb
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", default="MsPacmanNoFrameskip-v4")
-parser.add_argument("--iterations", default=1, type=int)
-parser.add_argument("--video_log_freq", default=5, type=int)
-
+parser.add_argument("--iterations", default=30, type=int)
+parser.add_argument("--video_log_freq", default=10, type=int)
+parser.add_argument("--num_gpus", default=2, type=int)
+parser.add_argument("--num_workers", default=8, type=int)
+parser.add_argument("--num_envs_per_worker", default=8, type=int)
+parser.add_argument("--n_step", default=4, type=int)
+parser.add_argument("--lr", default=0.00016, type=float)
+parser.add_argument("--eps_final", default=0.015, type=float)
+parser.add_argument("--eps_timesteps", default=200000, type=int)
+parser.add_argument("--timesteps_per_iteration", default=38000, type=int)
+parser.add_argument("--target_network_update_freq", default=48000, type=int)
+parser.add_argument("--rollout_fragment_length", default=64, type=int)
+parser.add_argument("--batch_size", default=64, type=int)
+parser.add_argument("--buffer_size", default=1000000, type=int)
 
 
 def build_training_config(args):
@@ -19,22 +31,22 @@ def build_training_config(args):
     config['dueling'] = 'true'
     config['num_atoms'] = 1
     config['noisy'] = 'false'
-    config['n_step'] = 3
-    config['lr'] = 0.0001
+    config['n_step'] = args.n_step
+    config['lr'] = args.lr
     config['adam_epsilon'] = 0.00015
     config['hiddens'] = [512]
-    config['buffer_size'] = 1000000
-    config['exploration_config'] = {'final_epsilon':0.01, 'epsilon_timesteps':200000}
+    config['buffer_size'] = args.buffer_size
+    config['exploration_config'] = {'final_epsilon':args.eps_final, 'epsilon_timesteps':args.eps_timesteps}
     config['prioritized_replay_alpha'] = 0.5
     config['final_prioritized_replay_beta'] = 1.0
     config['prioritized_replay_beta_annealing_timesteps'] = 2000000
-    config['num_gpus'] = 2
-    config['num_workers'] = 30
-    config['num_envs_per_worker'] = 1
-    config['rollout_fragment_length'] = 20
-    config['train_batch_size'] = 128
-    config['target_network_update_freq'] = 50000
-    config['timesteps_per_iteration'] = 25000
+    config['num_gpus'] = args.num_gpus
+    config['num_workers'] = args.num_workers
+    config['num_envs_per_worker'] = args.num_envs_per_worker
+    config['rollout_fragment_length'] = args.rollout_fragment_length
+    config['train_batch_size'] = args.batch_size
+    config['target_network_update_freq'] = args.target_network_update_freq
+    config['timesteps_per_iteration'] = args.timesteps_per_iteration
     config['monitor'] = 'true'
     return config
 
@@ -57,11 +69,10 @@ def build_log_config(args, config):
 
 def setup_wandb(args, log_config):
     wandb.init(
-        project='pacman', notes='getting setup',
+        project='pacman', notes='rllib',
         tags=['apex', 'rllib', 'dev run'], config=log_config
     )
     
-
     
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -79,10 +90,16 @@ if __name__ == "__main__":
     print("Exp dir detected: {}".format(expdir))
     
     # Begin training
+    timesteps = 0 
     for i in range(args.iterations):
+        start_time = time.time()
+
         result = trainer.train()
         print("Finished iter {}".format(i), result)
 
+        elapsed_time = time.time() - start_time
+        current_steps = result['timesteps_total'] - timesteps
+        
         # Monitor results
         wandb.log(
             {
@@ -91,10 +108,14 @@ if __name__ == "__main__":
                 'timesteps_total':result['timesteps_total'],
                 'learner_dequeue_time_ms':result['timers']['learner_dequeue_time_ms'],
                 'learner_grad_time_ms':result['timers']['learner_grad_time_ms'],
-                'learner_overall_time_ms':result['timers']['learner_overall_time_ms']
+                'learner_overall_time_ms':result['timers']['learner_overall_time_ms'],
+                'elapsed_time':elapsed_time,
+                'steps_per_unit_time':float(current_steps / elapsed_time)
             }
         )
 
+        timesteps = result['timesteps_total']
+        
         # Upload the new videos
         videos = glob.glob(expdir + "/*.mp4")
         print("Videos:", videos)
